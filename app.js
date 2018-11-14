3a1f2b9cc84733f57d7e49850e882f93a83c26e7
Martijn Vegter <mail@mvegter.me>
2018-11-14 22:31:41 +0100

    QuickFix.

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const controllers = require('./controllers');
+const io = require('socket.io')();
+const debug = require('debug')('RRS:Epicenter');
 
 const app = express();
+const port = 3000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(controllers);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
+const connections = [];
+io.on('connection', (socket) => {
+  socket.emit('identify', (type) => {
+    connections.push({
+      id: socket.id,
+      type,
+    });
+  });
 });
 
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/', (req, res) => res.send(connections));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(port, () => debug(`Example app listening on port ${port}!`));
+io.listen(8080);
